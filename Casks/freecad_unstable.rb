bb95e92e4b12c3969239f47f13ff048c19ea3b6c
Matthias Berse <matthias.berse@gmail.com>
2014-03-22 16:26:33 +0100

    unstable version of freecad since the latest stable would no start on my mavericks install.

---
+++ Casks/freecad_unstable.rb
@@ -0,0 +1,7 @@
+class Freecad_unstable < Cask
+  url 'http://downloads.sourceforge.net/project/free-cad/FreeCAD%20MacOSX/FreeCAD%200.14%20Unstable/FreeCAD_0.14.3052_x64.zip'
+  homepage 'http://sourceforge.net/projects/free-cad/'
+  version '0.14'
+  sha256 '2c3759ae17251cd8bc98f6b3d4747df8add9a75dce6ceddf07137affe98544f1'
+  link 'FreeCAD.app'
+end
